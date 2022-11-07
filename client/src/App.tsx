11ae43b31e3c2fb6187c7c3eeb85d7b3bd411d80
jjenna <jenna@solidware.io>
2022-11-07 15:15:40 +0900

    eject create-react-app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,52 @@
+import { useEffect, useState } from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
 import './App.css';
 
+console.log(process.env.REACT_APP_API_KEY)
+// auth setting
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN
+}
+firebase.initializeApp(firebaseConfig);
+
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  }
+};
+
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+      setIsSignedIn(!!user);
+    });
+    return () => unregisterAuthObserver();
+  }, []);
+
   return (
-    <div className="App">
-      <div>helloworld</div>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Firebase UI App</h1>
+      {isSignedIn ? (
+        <div>
+          <p>Welcome {firebase.auth().currentUser?.displayName}! You are now signed-in!</p>
+          <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+        </div>
+      ) : (
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      )}
     </div>
   );
 }
